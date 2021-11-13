559ccc635d3ab22621a718f72b6a95afe471abee
Ben Hauser <ben@hauser.id>
2021-11-13 18:52:32 +0400

    chore: update burner scripts

--- scripts/burn_fees.py
+++ scripts/burn_fees.py
@@ -11,12 +11,12 @@ def main():
     admin.execute(
         swap,
         swap.withdraw_admin_fees.encode_input(),
-        {"from": deploy, "gas_price": "20 gwei", "required_confs": 0},
+        {"from": deploy, "required_confs": 0},
     )
     admin.execute(
         swap_btc,
         swap_btc.withdraw_admin_fees.encode_input(),
-        {"from": deploy, "gas_price": "20 gwei", "required_confs": 0},
+        {"from": deploy, "required_confs": 0},
     )
     history.wait()
 
@@ -26,21 +26,28 @@ def main():
         try:
             burner.burn(
                 swap.coins(i),
-                {"from": deploy, "gas_price": "20 gwei", "gas_limit": 2000000, "required_confs": 0},
+                {"from": deploy, "gas_limit": 2000000, "required_confs": 0},
             )
         except Exception:
             pass
 
     # burn renBTC fees
-    burner = Contract("0x5109abc063164d49c148a7ae970f631febbda4fa")
+    burner = Contract("0x5109Abc063164d49C148A7AE970F631FEBbDa4FA")
     for i in range(2):
         try:
             burner.burn(
                 swap_btc.coins(i),
-                {"from": deploy, "gas_price": "20 gwei", "gas_limit": 2000000, "required_confs": 0},
+                {"from": deploy, "gas_limit": 2000000, "required_confs": 0},
             )
         except Exception:
             pass
+
+    # burn tricrypto3
+    burner = Contract("0x43450Feccf936FbA3143e03F35D3Cc608D5fE1d2")
+    burner.burn(
+        "0xdAD97F7713Ae9437fa9249920eC8507e5FbB23d3",
+        {'from': deploy, 'gas_limit': 2000000, 'required_confs': 0}
+    )
 
     history.wait()
 
@@ -48,9 +55,7 @@ def main():
     usdc = Contract("0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174")
     bridge = Contract("0x4473243A61b5193670D1324872368d015081822f")
     amount = usdc.balanceOf(bridge)
-    tx = admin.execute(
-        bridge, bridge.withdraw.encode_input(usdc), {"from": deploy, "gas_price": "20 gwei"}
-    )
+    tx = admin.execute(bridge, bridge.withdraw.encode_input(usdc), {"from": deploy})
 
     print(f"Burning phase 1 complete!\nAmount: {amount/1e6:,.2f} USDC\nBridge txid: {tx.txid}")
     print("\nUse `brownie run exit --network mainnet` to claim on ETH once the checkpoint is added")
